d18a4b31e73ad4f587f362f18f8ca93b41e41e22
Manuel Ramirez Coronel <ra_cm@outlook.com>
2022-05-24 11:21:51 -0500

    initial plugin with plugin boilerplate

---
+++ includes/class-minimum-order-amount-for-woocommerce-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/racmanuel
+ * @since      1.0.0
+ *
+ * @package    Minimum_Order_Amount_For_Woocommerce
+ * @subpackage Minimum_Order_Amount_For_Woocommerce/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Minimum_Order_Amount_For_Woocommerce
+ * @subpackage Minimum_Order_Amount_For_Woocommerce/includes
+ * @author     Manuel Ramirez Coronel <ra_cm@outlook.com>
+ */
+class Minimum_Order_Amount_For_Woocommerce_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-minimum-order-amount-for-woocommerce-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/racmanuel
+ * @since      1.0.0
+ *
+ * @package    Minimum_Order_Amount_For_Woocommerce
+ * @subpackage Minimum_Order_Amount_For_Woocommerce/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Minimum_Order_Amount_For_Woocommerce
+ * @subpackage Minimum_Order_Amount_For_Woocommerce/includes
+ * @author     Manuel Ramirez Coronel <ra_cm@outlook.com>
+ */
+class Minimum_Order_Amount_For_Woocommerce_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
